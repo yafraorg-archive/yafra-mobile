6c48a952f79f32dd55eed460bc72be15eba1fa64
yafraorg <yafraorg@gmail.com>
2015-07-25 17:13:00 +0200

    reworked the build script for shippable due to sudo's and npm version

--- build-shippable.sh
+++ build-shippable.sh
@@ -7,10 +7,40 @@
 #export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 echo "Make sure you set AndroidManifest.xml android:debuggable to false!"
-NODEPATH=/home/shippable/workspace/src/github.com/yafraorg/yafra-mobile/node_modules
-cd ionic
-$NODEPATH/ionic/bin/ionic platform add android
-$NODEPATH/ionic/bin/ionic resources
-$NODEPATH/gulp/bin/gulp.js
-$NODEPATH/ionic/bin/ionic build android
-echo "done - save in /work"
+
+#
+# bower install / upgrade
+#
+echo ' '
+echo 'start bower ngCordova update now'
+# ng-cordova install/update
+bower install ngCordova
+bower install ng-cordova-oauth
+cp bower_components/ngCordova/dist/ng-cordova.js ionic/www/lib/ngCordova
+cp bower_components/ngCordova/dist/ng-cordova.min.js ionic/www/lib/ngCordova
+cp bower_components/ngCordova/dist/ng-cordova-mocks.js tests-helper/
+cp bower_components/ngCordova/dist/ng-cordova-mocks.min.js tests-helper/
+cp bower_components/ng-cordova-oauth/dist/ng-cordova-oauth.js ionic/www/lib/ngCordova
+cp bower_components/ng-cordova-oauth/dist/ng-cordova-oauth.min.js ionic/www/lib/ngCordova
+echo ' '
+echo 'bower update done - list bower installations:'
+bower list
+
+
+#
+# install / update ionic project (resources, platforms, ...)
+#
+echo ' '
+echo 'make ionic init'
+cd ionic/
+ionic platform add android
+ionic resources
+ionic lib
+echo 'you need to update angular and angular-resource manually and'
+echo 'with the same angular release as this ionic relese uses - take a starter template and copy the angular directories over'
+echo ' '
+echo 'Ionic update DONE'
+node_modules/.bin/gulp
+ionic build android
+cd ..
+echo "done - mobile app build for android"
