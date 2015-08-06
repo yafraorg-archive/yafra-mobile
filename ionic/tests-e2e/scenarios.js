4a1c0e61490ae3c64953c69d670ba8b77cd4311f
Martin Weber Nissle <mwn@Martins-MacBook-Pro.local>
2015-08-06 11:43:27 +0200

    fix: updated the shippable config to complete with protractor
    Added selenium update and changed scenarios of protractor
    
    Work in progress

--- ionic/tests-e2e/scenarios.js
+++ ionic/tests-e2e/scenarios.js
@@ -2,28 +2,39 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('default page check', function() {
+describe('Yafra Mobile App', function() {
 
-  browser.get('index.html');
+    browser.get('index.html');
 
-  it('should automatically redirect to /yafra/home when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/yafra/home");
-  });
+    it('should automatically redirect to /yafra/home when location hash/fragment is empty', function() {
+        expect(browser.getLocationAbsUrl()).toMatch("/yafra/home");
+        });
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('YAFRA');
-  });
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('YAFRA');
+        });
+
+    it('should change to git issues', function () {
+        element(by.css('.yafragitmenu')).click();
+        expect(browser.getCurrentUrl()).toMatch("/yafra/git");
+        });
+
+    describe('git issue page', function() {
+
+        beforeEach(function() {
+            browser.get('index.html#/yafra/git');
+            });
+
+        it('should change to git issues', function () {
+            expect(browser.getCurrentUrl()).toMatch("/yafra/git");
+            });
+
+        it('should have a title', function() {
+            expect(browser.getTitle()).toEqual('Git Issues');
+            });
+
+    });
+
 
 });
 
-/*
-describe('git page check', function() {
-
-    browser.get('index.html#/yafra/git');
-
-        it('should change to git issues', function () {
-            //element(by.css('.yafragitmenu')).click();
-            expect(browser.getCurrentUrl()).toMatch("/yafra/git");
-        });
-    });
-*/
